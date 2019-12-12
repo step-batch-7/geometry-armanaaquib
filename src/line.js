2936ecb3f95ea0194040902554d22f53dab2139f
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2019-12-12 15:57:52 +0530

    added isEqual method in Line Class

--- src/line.js
+++ src/line.js
@@ -16,6 +16,15 @@ class Line {
             `End Point(x,y): (${this.endPoint.x},${this.endPoint.y})`
         );
     }
+
+    isEqual(otherLine) {
+        let isEqual = this.startPoint.x === otherLine.startPoint.x;
+        isEqual = isEqual && this.startPoint.y === otherLine.startPoint.y;
+        isEqual = isEqual && this.endPoint.x === otherLine.endPoint.x;
+        isEqual = isEqual && this.endPoint.y === otherLine.endPoint.y;
+
+        return isEqual;
+    }
 }
 
 module.exports = Line;
