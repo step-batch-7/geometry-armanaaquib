0f7f79b2324a23b7b9290e95824924c9c694732a
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2019-12-13 10:08:48 +0530

    modified toString() representation

--- src/line.js
+++ src/line.js
@@ -1,20 +1,11 @@
 class Line {
     constructor(startPoint, endPoint) {
-        this.startPoint = {};
-        this.endPoint = {};
-
-        this.startPoint.x = startPoint.x;
-        this.startPoint.y = startPoint.y;
-
-        this.endPoint.x = endPoint.x;
-        this.endPoint.y = endPoint.y;
+        this.startPoint = { ...startPoint };
+        this.endPoint = { ...endPoint };
     }
 
     toString() {
-        return (
-            `Start Point(x,y): (${this.startPoint.x},${this.startPoint.y})\n` +
-            `End Point(x,y): (${this.endPoint.x},${this.endPoint.y})`
-        );
+        return `Line start(${this.startPoint.x},${this.startPoint.y}),end(${this.endPoint.x},${this.endPoint.y})`;
     }
 
     isEqual(otherLine) {
--- test/testLine.js
+++ test/testLine.js
@@ -4,7 +4,7 @@ const Line = require("../src/line");
 describe("line", function() {
     describe("toString", function() {
         it("should return points which is passed", function() {
-            const expectedValue = "Start Point(x,y): (1,2)\nEnd Point(x,y): (2,3)";
+            const expectedValue = "Line start(1,2),end(2,3)";
             const actualValue = new Line({ x: 1, y: 2 }, { x: 2, y: 3 }).toString();
 
             assert.strictEqual(actualValue, expectedValue);
@@ -23,9 +23,6 @@ describe("line", function() {
         });
 
         it("should return false if lines are not same", function() {
-            const startPoint = { x: 1, y: 2 };
-            const endPoint = { x: 2, y: 3 };
-
             const lineOne = new Line({ x: 1, y: 2 }, { x: 2, y: 3 });
             const lineTwo = new Line({ x: 2, y: 2 }, { x: 2, y: 4 });
 
