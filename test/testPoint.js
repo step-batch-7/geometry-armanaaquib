1b9b21070def60bbaea01e117edefad73b399366
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2019-12-14 12:44:00 +0530

    added visit method in Point class

--- test/testPoint.js
+++ test/testPoint.js
@@ -8,4 +8,22 @@ describe("Point", function() {
             assert.deepStrictEqual(point.toString(), "[Point @(2,3)]");
         });
     });
+
+    describe("#visit()", function() {
+        it("should work for add function", function() {
+            const point = new Point(2, 3);
+            assert.strictEqual(
+                point.visit((x, y) => x + y),
+                5
+            );
+        });
+
+        it("should work for multiply function", function() {
+            const point = new Point(2, 3);
+            assert.strictEqual(
+                point.visit((x, y) => x * y),
+                6
+            );
+        });
+    });
 });
