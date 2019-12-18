626703ebbae94f2078b939fb3c1665cc4f059fc0
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2019-12-18 10:10:31 +0530

    added area property in Rectangle Class

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,5 +1,21 @@
 const Point = require("./point");
 
+const getVertexBandD = function(vertexA, vertexC) {
+    const vertexB = new Point(vertexC.x, vertexA.y);
+    const vertexD = new Point(vertexA.x, vertexC.y);
+
+    return { vertexB, vertexD };
+};
+
+const getDimensions = function(vertexA, vertexC) {
+    const { vertexB, vertexD } = getVertexBandD(vertexA, vertexC);
+
+    const length = Math.abs(vertexA.x - vertexB.x);
+    const width = Math.abs(vertexA.y - vertexD.y);
+
+    return { length, width };
+};
+
 class Rectangle {
     constructor(vertexA, vertexC) {
         this.vertexA = new Point(vertexA.x, vertexA.y);
@@ -16,6 +32,12 @@ class Rectangle {
         const diagonalEndString = `(${this.vertexC.x},${this.vertexC.y})`;
 
         return `[Rectangle ${diagonalStartString} to ${diagonalEndString}]`;
+    }
+
+    get area() {
+        const { length, width } = getDimensions(this.vertexA, this.vertexC);
+
+        return length * width;
     }
 }
 
--- test/testRectangle.js
+++ test/testRectangle.js
@@ -18,4 +18,44 @@ describe("Rectangle", function() {
             assert.deepStrictEqual(rectangle.toString(), "[Rectangle (1,1) to (2,3)]");
         });
     });
+
+    describe("#area", function() {
+        it("should return area 0 if diagonal is parallel to x-axis", function() {
+            const rectangle = new Rectangle(new Point(1, 1), new Point(5, 1));
+
+            assert.strictEqual(rectangle.area, 0);
+        });
+
+        it("should return area 0 if diagonal is parallel to y-axis", function() {
+            const rectangle = new Rectangle(new Point(1, 1), new Point(1, 5));
+
+            assert.strictEqual(rectangle.area, 0);
+        });
+
+        it("should return area if diagonal is incline", function() {
+            const rectangle = new Rectangle(new Point(1, 1), new Point(5, 5));
+
+            assert.strictEqual(rectangle.area, 16);
+        });
+    });
+
+    describe("#perimeter", function() {
+        it("should return perimeter if diagonal is parallel to x-axis", function() {
+            const rectangle = new Rectangle(new Point(1, 1), new Point(5, 1));
+
+            assert.strictEqual(rectangle.perimeter, 8);
+        });
+
+        it("should return perimeter if diagonal is parallel to y-axis", function() {
+            const rectangle = new Rectangle(new Point(1, 1), new Point(1, 5));
+
+            assert.strictEqual(rectangle.perimeter, 8);
+        });
+
+        it("should return perimeter if diagonal is incline", function() {
+            const rectangle = new Rectangle(new Point(1, 1), new Point(4, 5));
+
+            assert.strictEqual(rectangle.perimeter, 14);
+        });
+    });
 });
